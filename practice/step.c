506cd984d6c86a561759bc639a926ea2375df68f
shintaro kohtake <shintarokohtake@shintaros-MacBook-Air.local>
2025-07-13 17:29:16 +0900

    add submodule

---
+++ practice/step.c
@@ -0,0 +1,39 @@
+# include "./solong.h"
+
+typedef struct s_game
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_game;
+
+int	close_window_hook(t_game *game)
+{
+
+}
+
+
+
+
+
+
+
+
+
+int	main()
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	if (mlx_ptr == NULL)
+	{
+		exit(1);
+	}
+	win_ptr = mlx_new_window(mlx_ptr, 800, 600,"step window" );
+	if (win_ptr == NULL)
+	{
+		exit(1);
+	}
+	mlx_loop(mlx_ptr);
+	return 0;
+}
