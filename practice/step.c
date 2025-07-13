5bd5427c130c9fec83d495f0972a9d15717aee81
shintaro kohtake <shintarokohtake@shintaros-MacBook-Air.local>
2025-07-13 21:11:08 +0900

    add step2

--- practice/step.c
+++ practice/step.c
@@ -8,11 +8,39 @@ typedef struct s_game
 
 int	close_window_hook(t_game *game)
 {
+	ft_putendl_fd(" --- window close hook --- ",STDOUT_FILENO);
+	if(game->mlx_ptr)
+		mlx_destroy_window(game->mlx_ptr, game->win_ptr);
+	exit(0);
+}
+
+int	key_press_hook(int keycode, t_game *game)
+{
+	ft_putendl_fd(" --- key press hook --- ",STDOUT_FILENO);
+	if(keycode == 65307)	//Linux
+	// if(keycode == 53)	//mac
+	{
+		ft_putendl_fd(" --- ESC key : window close hook --- ",STDOUT_FILENO);
+		close_window_hook(game);
+	}
+	return 0;
 }
 
 int main()
 {
+	t_game game;
+
 	ft_putendl_fd(" --- main functions --- ", STDOUT_FILENO);
+	game.mlx_ptr = mlx_init();
+	if(game.mlx_ptr == NULL)
+		return(1);
+	game.win_ptr = mlx_new_window(game.mlx_ptr, 800, 600, "STEP window");
+	if(game.win_ptr == NULL)
+		return(1);
+	ft_putendl_fd(" --- open a window --- ", STDOUT_FILENO);
+	mlx_hook(game.win_ptr, 17, 1L << 17, close_window_hook, &game);
+	mlx_hook(game.win_ptr, 2, 1L << 0, key_press_hook, &game);
+	mlx_loop(game.mlx_ptr);
 	return 0;
 }
 
