9771bb1c8ec58b097c1e33129d9fb3834e924976
shintaro kohtake <shintarokohtake@shintaros-MacBook-Air.local>
2025-07-26 23:04:38 +0900

    remove extra files

--- practice/solong.h
+++ practice/solong.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   solong.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skohtake <skohtake@student.42.fr>          +#+  +:+       +#+        */
+/*   By: shintarokohtake <shintarokohtake@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 20:23:07 by skohtake          #+#    #+#             */
-/*   Updated: 2025/07/26 22:00:51 by skohtake         ###   ########.fr       */
+/*   Updated: 2025/07/26 22:50:59 by shintarokoh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,15 @@
 
 typedef struct s_map
 {
-	char	**data;
-	int		width;
-	int		height;
-	int		player_count;
-	int		exit_count;
-	int		collectible_count;
-	int		player_pos_x;
-	int		player_pos_y;
-}			t_map;
+	char				**data;
+	int					width;
+	int					height;
+	int					player_count;
+	int					exit_count;
+	int					collectible_count;
+	int					player_pos_x;
+	int					player_pos_y;
+}						t_map;
 
 // typedef	struct s_img
 // {
@@ -50,33 +50,33 @@ typedef struct s_map
 
 typedef struct s_game
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	t_map	map;
-	int		move_count;
+	void				*mlx_ptr;
+	void				*win_ptr;
+	t_map				map;
+	int					move_count;
 	// t_img	img;
-	void	*img_wall;
-	void	*img_floor;
-	void	*img_player;
-	void	*img_collectible;
-	void	*img_exit;
-	int		img_width;
-	int		img_height;
-}			t_game;
+	void				*img_wall;
+	void				*img_floor;
+	void				*img_player;
+	void				*img_collectible;
+	void				*img_exit;
+	int					img_width;
+	int					img_height;
+}						t_game;
 
-typedef struct s_queue_node
-{
-	int					x;
-	int					y;
-	struct s_queue_node	*next;
-}						t_queue_node;
+// typedef struct s_queue_node
+// {
+// 	int					x;
+// 	int					y;
+// 	struct s_queue_node	*next;
+// }						t_queue_node;
 
-typedef struct s_queue
-{
-	t_queue_node		*front;
-	t_queue_node		*rear;
-	int					size;
-}						t_queue;
+// typedef struct s_queue
+// {
+// 	t_queue_node		*front;
+// 	t_queue_node		*rear;
+// 	int					size;
+// }						t_queue;
 
 # define TILE_SIZE 32
 
@@ -107,39 +107,42 @@ typedef struct s_queue
 
 # endif
 
-void	error_exit(char *message);
-void	read_map_from_file(const char *file_path, t_game *game);
-char	**ft_stradd(char **double_array, char *new_str);
-void	free_double_ptr(char **ptr, size_t count);
+void					error_exit(char *message);
+void					read_map_from_file(const char *file_path, t_game *game);
+char					**ft_stradd(char **double_array, char *new_str);
+void					free_double_ptr(char **ptr, size_t count);
 
-void	check_walls(t_map *map);
-void	check_elements(t_map *map);
-void	validate_map(t_map *map);
+void					check_walls(t_map *map);
+void					check_elements(t_map *map);
+void					validate_map(t_map *map);
 
 // flood_fill_validation.c
-void	my_flood_fill(char **grid, int width, int height, int x, int y);
-bool	is_valid_position_to_fill(char **grid, int width, int height, int x, int y);
-bool	map_includes_specific_char(char **grid, int width, int height, char c);
-void	validate_playability(t_game *game);
+void					my_flood_fill(char **grid, int width, int height, int x,
+							int y);
+bool					is_valid_position_to_fill(char **grid, int width,
+							int height, int x, int y);
+bool					map_includes_specific_char(char **grid, int width,
+							int height, char c);
+void					validate_playability(t_game *game);
 
 // player_moves.c
-int		is_valid_move_position(t_map *map, int x, int y);
-void	handle_game_clear(t_game *game);
-int		move_player(t_game *game, int dx, int dy);
+int						is_valid_move_position(t_map *map, int x, int y);
+void					handle_game_clear(t_game *game);
+int						move_player(t_game *game, int dx, int dy);
 
 // hooks.c (または main.c)
-int		close_window_hook(t_game *game);
-int		key_press_hook(int keycode, t_game *game);
+int						close_window_hook(t_game *game);
+int						key_press_hook(int keycode, t_game *game);
 
 // render.c
-int		render_map(t_game *game);
-int		draw_stuff(t_game *game);
-int		draw_image(t_game *game);
+int						render_map(t_game *game);
+int						draw_stuff(t_game *game);
+int						draw_image(t_game *game);
 
 // utility.c (または general utilities)
-void	my_print_map(char **mapdata);
-void	free_map_data(t_map *map);
-void	destroy_game_resources(t_game *game);
+void					my_print_map(char **mapdata);
+void					free_map_data(t_map *map);
+void					destroy_game_resources(t_game *game);
 
 // // movement keycode for Linux (X11)
 // # define KEY_ESC 65307
